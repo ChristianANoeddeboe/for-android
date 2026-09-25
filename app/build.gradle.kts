cebd20c7b1ffbef89b1082f8acb5b4b55826ae3f
ChristianANoeddeboe <christian@noddeboe.dk>
2026-09-25 21:18:22 +0200

    chore: bump to 1.7.2-selfhosted.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         applicationId = "chat.revolt"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = Integer.parseInt("001_007_007".replace("_", ""), 10)
-        versionName = "1.7.2-selfhosted.6"
+        versionCode = Integer.parseInt("001_007_008".replace("_", ""), 10)
+        versionName = "1.7.2-selfhosted.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
